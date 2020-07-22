9d3e29da44a2836f932ba9ea7cc734dc0ef23f03
Ricardo Olarte <raop155@hotmail.com>
2020-07-22 12:41:27 -0500

    useState hook

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+
+const App = (props) => {
+  const { startCount } = props;
+  const [count, setCount] = new useState(startCount);
+
+  const addCount = () => {
+    setCount(count + 1);
+  }
+
+  const minusCount = () => {
+    setCount(count - 1);
+  }
+
+  const resetCount = () => {
+    setCount(startCount);
+  }
+
+  return (
+    <div>
+      <p>
+        The current count is {count}
+      </p>
+      <button onClick={minusCount}>-1</button>
+      <button onClick={resetCount}>Reset</button>
+      <button onClick={addCount}>+1</button>
+    </div>
+  )
+}
+
+App.defaultProps = {
+  startCount: 0
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App startCount={0} />
   </React.StrictMode>,
   document.getElementById('root')
 );
